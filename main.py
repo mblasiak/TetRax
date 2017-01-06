3161f9e04a47b93e151aa1632844e2ccb8c543fa
Miłosz Blasiak <milblasiak@gmail.com>
2017-01-06 20:02:27 +0100

    Testy obj mb .egg itp

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("fupo")
+
+
+from direct.showbase.ShowBase import ShowBase
+
+
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.plant=loader.loadModel("SP.mb")
+        self.plant.reparentTo(render)
+
+app = MyApp()
+app.run()

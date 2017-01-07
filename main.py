e75ece9d92113d678631e9bf99ff3537d6169827
Miłosz Blasiak <milblasiak@gmail.com>
2017-01-07 01:32:53 +0100

    Test textur i 3d

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from direct.showbase.ShowBase import ShowBase
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.plant=loader.loadModel("SP.mb")
+        self.plant=loader.loadModel("PS.egg")
         self.plant.reparentTo(render)
 
 app = MyApp()
